ce07a7ad4c8498e9f153ddb63b83955c59508e76
Dzmitry <dimchikovich1@gmail.com>
2023-08-21 01:13:30 +0200

    Project created

---
+++ HWKurylenka_14_NavigationApp/HWKurylenka_14_NavigationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWKurylenka_14_NavigationApp
+//
+//  Created by MACBOOK PRO 15,4 2017 ROK on 21/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
